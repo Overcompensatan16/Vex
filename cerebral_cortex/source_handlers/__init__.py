f62bef625f3049a55d16fab2e37ea8cc0ff23af1
Overcompensatan16 <148434423+Overcompensatan16@users.noreply.github.com>
2025-08-04 17:18:20 -0400

    Delete cerebral_cortex/source_handlers/__init__.py

--- cerebral_cortex/source_handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Collective source handler interface."""
-
-from cerebral_cortex.source_handlers.external_loaders.wiki_handler import download_and_clean
-from cerebral_cortex.source_handlers.external_loaders.arxiv_handler import fetch_subject
-
-__all__ = ["download_and_clean", "fetch_subject"]
