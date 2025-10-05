689c5c8b58c372f6b9ab65a03daaeef657ebb164
Overcompensatan16 <148434423+Overcompensatan16@users.noreply.github.com>
2025-10-05 10:27:10 -0400

    Delete body directory

--- body/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Body subsystem modules for Vex.
-
-This package collects musculoskeletal routing modules that prepare
-signals from the nervous-system simulation for rig-friendly outputs.
-"""
-
-__all__ = [
-    "limbs",
-]
